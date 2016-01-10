82bfcb7bdd5b6e3461d591dea399722d8b94dc44
hkdnet <hkdnet@users.noreply.github.com>
2016-01-10 11:50:12 +0900

    add User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'yaml'
+
+YAML.load_file('db/users.yml').each do |u|
+  User.create(name: u['name'], age: u['age'])
+end
